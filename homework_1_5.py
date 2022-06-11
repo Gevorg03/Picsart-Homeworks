1940be99078a7e80f80fdca73035404b9d1bd636
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-06-11 11:53:24 +0400

    Update homework_1_5.py

--- homework_1_5.py
+++ homework_1_5.py
@@ -1,1 +1,10 @@
-
+def sum_of_two_num(num1,num2):
+    #Calculate the sum of integers from num1 to num2 accepting that num1 < num2
+    
+    sum = 0
+    while num1 < num2:
+        sum+=num1
+        num1+=1
+    return sum  
+  
+print(sum_of_two_num(3,4))
