8bebb57bb331c424b9a0c34706d1a91aa6165ffb
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-06-15 18:25:11 +0400

    Create f_contain.py

---
+++ f_contain.py
@@ -0,0 +1,11 @@
+def contain(data,val):
+    #is val contained in list or no?
+    
+    i = 0
+    while i < len(data):
+        if data[i] == val:
+            return True
+        i+=1
+    return False   
+    
+print(contain([1,2,3,4],3))    
