adcf6b4522e69766215d9246bf830dcf943f0c13
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-06-16 11:15:55 +0400

    Create f_reverse.py

---
+++ f_reverse.py
@@ -0,0 +1,10 @@
+def reverse(data):
+    #invert list
+    
+    i = 0
+    while i < len(data)//2:
+        data[i],data[len(data)-i-1] = data[len(data)-i-1],data[i]
+        i+=1    
+    return data
+    
+print(reverse([1,2,3,4,5,6,7])) 
