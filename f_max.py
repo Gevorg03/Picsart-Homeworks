43ac2839e9f565b686d73110b35c4aeaf5a5a99a
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-06-16 11:18:43 +0400

    Create f_max.py

---
+++ f_max.py
@@ -0,0 +1,12 @@
+def max(data):
+    #return the maximum element of list
+    
+    i = 1
+    max = data[0]
+    while i < len(data):
+        if max < data[i]:
+            max = data[i]
+        i+=1    
+    return max
+    
+print(max([14,20,99,30,5,8]))  
