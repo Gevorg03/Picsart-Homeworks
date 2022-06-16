945bb55842724e4d9582ad09a3dcee9b184a2b0b
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-06-16 11:19:53 +0400

    Create f_min.py

---
+++ f_min.py
@@ -0,0 +1,12 @@
+def min(data):
+    #return the minimum element of list
+    
+    i = 1
+    min = data[0]
+    while i < len(data):
+        if min > data[i]:
+            min = data[i]
+        i+=1    
+    return min
+    
+print(min([14,20,99,30,5,8])) 
