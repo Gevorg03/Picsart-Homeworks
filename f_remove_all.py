f4018bb4332ef77385565800adb66f500e1b7ef7
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-06-16 11:33:57 +0400

    Update f_remove_all.py

--- f_remove_all.py
+++ f_remove_all.py
@@ -1,11 +1,14 @@
 def remove_all(data,value):
     #delete the elements, that are equal in value
     
+    size = len(data)
     i = 0
     while i < len(data):
         if data[i] == value:
             data.remove(value)
-        i+=1    
-            
+        i+=1   
+    
+    if(size == len(data)):
+        raise ValueError("data.remove(x): x not in list")
     return data
-print(remove_all([1,2,3,4,1,4],4))  
+print(remove_all([1,2,3,4,1,4],5))
