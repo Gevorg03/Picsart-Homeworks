81079d8a1462f9a122e34a66a2b87d47b3f07b5e
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-06-26 15:26:48 +0400

    Update gorcnakan3.py

--- gorcnakan3.py
+++ gorcnakan3.py
@@ -18,3 +18,21 @@ def reverse_word(word):
     return reversed_word
 _word = input("Input a word: ")
 print(reverse_word(_word))
+
+def median(data):
+    mid = len(data) // 2
+    return (data[mid] + data[mid-1]) / 2 if len(data) % 2 == 0 else data[mid] 
+print("Median =", median([1, 2, 3, 4, 5, 6]))    
+
+def count_days(year, month):
+    if month in (4, 6, 9, 11):
+        return 30
+    elif month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    elif month == 2 and year % 4 == 0:
+        return 29
+    return 28    
+year = int(input("Input the year: "))
+month = int(input("Input the month: "))
+print(count_days(year, month), "day")
+ 
